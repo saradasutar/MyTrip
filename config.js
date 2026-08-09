23c2da8f16c9adac04e1a9e03caf48ce395eb5d2
Sarada <saradawrites@gmail.com>
2026-08-09 20:34:01 +0530

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /* Optional: paste your deployed Google Apps Script /exec URL below.
    If left blank, the dashboard will ask the administrator to connect it. */
 window.MYTRIP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxvLAooeYUGCp9lspoA9pDDqgzZ5pkonwoTIs1XZzfGoCsoEwSP6BomtyiHGhiu0-aZXg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyEYRNWspstoD3ucDt8eVOoja9SsMWRE8jagSrG1in_qM1RRnNHK-0glHbBXbY_RVL4IQ/exec",
   APP_NAME: "MyTrip",
   DEFAULT_CURRENCY: "INR"
 };
